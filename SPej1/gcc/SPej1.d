699f0c0c1a30d593dd841f11782cb20782e8e1d3
MagoElectrico <103003699+MagoElectrico@users.noreply.github.com>
2026-05-28 23:38:16 -0400

    Delete SPej1 directory

--- SPej1/gcc/SPej1.d
+++
@@ -1,7 +0,0 @@
-gcc/SPej1.o: SPej1.c /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
- ../../../../inc/hw_ints.h ../../../../inc/hw_memmap.h \
- ../../../../inc/hw_types.h ../../../../driverlib/sysctl.h \
- ../../../../driverlib/gpio.h ../../../../driverlib/uart.h \
- ../../../../driverlib/timer.h ../../../../driverlib/interrupt.h \
- ../../../../driverlib/pin_map.h
